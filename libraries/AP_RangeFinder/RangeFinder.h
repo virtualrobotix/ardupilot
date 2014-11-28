c562f44936266f262664cf7983ca390a88e92876
LukeMike <luca.micheletti.laser@gmail.com>
2014-11-28 15:53:20 +0100

    BUILD: update for building all firmware's version for all boards + VR Brain 4.5 Standard and PWM + VR Brain 5.1 Standard and PWM + VR Brain 5.1 Pro Standard and PWM + VR Micro Brain 5.1 Standard and PWM + VR Micro Brain 5.2 Standard

--- libraries/AP_RangeFinder/RangeFinder.h
+++ libraries/AP_RangeFinder/RangeFinder.h
@@ -38,11 +38,12 @@ public:
 
     // RangeFinder driver types
     enum RangeFinder_Type {
-        RangeFinder_TYPE_NONE   = 0,
-        RangeFinder_TYPE_ANALOG = 1,
-        RangeFinder_TYPE_MBI2C  = 2,
-        RangeFinder_TYPE_PLI2C  = 3,
-        RangeFinder_TYPE_PX4    = 4
+        RangeFinder_TYPE_NONE    = 0,
+        RangeFinder_TYPE_ANALOG  = 1,
+        RangeFinder_TYPE_MBI2C   = 2,
+        RangeFinder_TYPE_PLI2C   = 3,
+        RangeFinder_TYPE_PX4     = 4,
+        RangeFinder_TYPE_VRBRAIN = 5
     };
 
     enum RangeFinder_Function {
