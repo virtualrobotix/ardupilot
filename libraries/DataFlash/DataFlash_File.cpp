e7f5c2f5a39ed79498db1d1986a6bfe874812564
LukeMike <luca.micheletti.laser@gmail.com>
2014-12-10 06:34:29 +0100

    VRBRAIN: added new boards VR Brain 5.2 Standard and Professional versions

--- libraries/DataFlash/DataFlash_File.cpp
+++ libraries/DataFlash/DataFlash_File.cpp
@@ -55,6 +55,8 @@ DataFlash_File::DataFlash_File(const char *log_directory) :
     _writebuf_chunk(512),
 #elif defined(CONFIG_ARCH_BOARD_VRBRAIN_V51)
     _writebuf_chunk(512),
+#elif defined(CONFIG_ARCH_BOARD_VRBRAIN_V52)
+    _writebuf_chunk(512),
 #elif defined(CONFIG_ARCH_BOARD_VRUBRAIN_V51)
     _writebuf_chunk(512),
 #elif defined(CONFIG_ARCH_BOARD_VRUBRAIN_V52)
