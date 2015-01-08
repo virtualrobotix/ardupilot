cf9bbdf2d4cb78d00c7834ae6220bf68db067dad
Emile Castelnuovo <emile.castelnuovo@gmail.com>
2015-01-08 18:08:24 +0100

    AP_HAL_VRBRAIN: added micros64() and millis64()

--- libraries/AP_HAL_VRBRAIN/Scheduler.cpp
+++ libraries/AP_HAL_VRBRAIN/Scheduler.cpp
@@ -76,6 +76,16 @@ void VRBRAINScheduler::init(void *unused)
 	pthread_create(&_io_thread_ctx, &thread_attr, (pthread_startroutine_t)&VRBRAIN::VRBRAINScheduler::_io_thread, this);
 }
 
+uint64_t VRBRAINScheduler::micros64() 
+{
+    return hrt_absolute_time();
+}
+
+uint64_t VRBRAINScheduler::millis64() 
+{
+    return micros64() / 1000;
+}
+
 uint32_t VRBRAINScheduler::micros()
 {
     return (uint32_t)(hrt_absolute_time() - _sketch_start_time);
@@ -83,7 +93,7 @@ uint32_t VRBRAINScheduler::micros()
 
 uint32_t VRBRAINScheduler::millis()
 {
-    return hrt_absolute_time() / 1000;
+    return millis64() & 0xFFFFFFFF;
 }
 
 /**
@@ -106,9 +116,9 @@ void VRBRAINScheduler::delay_microseconds(uint16_t usec)
         perf_end(_perf_delay);
         return;
     }
-	uint32_t start = micros();
-    uint32_t dt;
-	while ((dt=(micros() - start)) < usec) {
+	uint64_t start = micros64();
+    uint64_t dt;
+	while ((dt=(micros64() - start)) < usec) {
 		up_udelay(usec - dt);
 	}
     perf_end(_perf_delay);
@@ -121,9 +131,9 @@ void VRBRAINScheduler::delay(uint16_t ms)
         return;
     }
     perf_begin(_perf_delay);
-	uint64_t start = hrt_absolute_time();
+	uint64_t start = micros64();
     
-    while ((hrt_absolute_time() - start)/1000 < ms && 
+    while ((micros64() - start)/1000 < ms && 
            !_vrbrain_thread_should_exit) {
         delay_microseconds_semaphore(1000);
         if (_min_delay_cb_ms <= ms) {
