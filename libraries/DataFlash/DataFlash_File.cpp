adec1850ec4c4b1a356646c5413757402255aa45
LukeMike <luca.micheletti.laser@gmail.com>
2015-05-13 21:28:39 +0200

    DataFlash: deleted configuration of VR Hero board

--- libraries/DataFlash/DataFlash_File.cpp
+++ libraries/DataFlash/DataFlash_File.cpp
@@ -74,8 +74,6 @@ DataFlash_File::DataFlash_File(DataFlash_Class &front,
     _writebuf_chunk(512),
 #elif defined(CONFIG_ARCH_BOARD_VRUBRAIN_V52)
     _writebuf_chunk(512),
-#elif defined(CONFIG_ARCH_BOARD_VRHERO_V10)
-    _writebuf_chunk(512),
 #else
     _writebuf_chunk(4096),
 #endif
