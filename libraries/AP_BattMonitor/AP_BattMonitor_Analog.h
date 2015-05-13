321ac564c0c7317a3690310de5871a86b53a4d9e
LukeMike <luca.micheletti.laser@gmail.com>
2015-05-13 21:25:51 +0200

    AP_BattMonitor: deleted configuration for VR Hero board

--- libraries/AP_BattMonitor/AP_BattMonitor_Analog.h
+++ libraries/AP_BattMonitor/AP_BattMonitor_Analog.h
@@ -62,11 +62,6 @@
  # define AP_BATT_VOLTDIVIDER_DEFAULT       10.1f
  # define AP_BATT_CURR_AMP_PERVOLT_DEFAULT  17.0f
 #endif
-#elif CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN && defined(CONFIG_ARCH_BOARD_VRHERO_V10)
- # define AP_BATT_VOLT_PIN                  100
- # define AP_BATT_CURR_PIN                  101
- # define AP_BATT_VOLTDIVIDER_DEFAULT       1.1f
- # define AP_BATT_CURR_AMP_PERVOLT_DEFAULT  17.0f
 #else
  # define AP_BATT_VOLT_PIN                  -1
  # define AP_BATT_CURR_PIN                  -1
