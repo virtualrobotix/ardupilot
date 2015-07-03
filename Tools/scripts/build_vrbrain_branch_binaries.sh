26cebc6f90fd0395541410d93971810fa8a2832b
LukeMike <luca.micheletti.laser@gmail.com>
2015-07-03 09:02:22 +0200

    Tools: updated scripts to build branch or project

--- Tools/scripts/build_vrbrain_branch_binaries.sh
+++ Tools/scripts/build_vrbrain_branch_binaries.sh
@@ -312,9 +312,9 @@ build_rover() {
     popd
 }
 
-for build in latest; do
-    build_arduplane $build
-    build_arducopter $build
+for build in beta; do
+    #build_arduplane $build
+    #build_arducopter $build
     build_rover $build
 done
 
