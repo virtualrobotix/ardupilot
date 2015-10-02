ac1be04f40198ed1de861eea9554b38dceba3790
LukeMike <luca.micheletti.laser@gmail.com>
2015-10-02 09:26:05 +0200

    AP_Notify: added new configuration for VR LX Brain

--- libraries/AP_Notify/AP_BoardLED.h
+++ libraries/AP_Notify/AP_BoardLED.h
@@ -61,6 +61,12 @@
  # define HAL_GPIO_C_LED_PIN        117
  # define HAL_GPIO_LED_ON           LOW
  # define HAL_GPIO_LED_OFF          HIGH
+#elif CONFIG_HAL_BOARD_SUBTYPE == HAL_BOARD_SUBTYPE_LINUX_VRLXBRAIN
+ # define HAL_GPIO_A_LED_PIN        16
+ # define HAL_GPIO_B_LED_PIN        16
+ # define HAL_GPIO_C_LED_PIN        16
+ # define HAL_GPIO_LED_ON           LOW
+ # define HAL_GPIO_LED_OFF          HIGH
 #elif CONFIG_HAL_BOARD == HAL_BOARD_VRBRAIN
  # define HAL_GPIO_A_LED_PIN        25
  # define HAL_GPIO_B_LED_PIN        26
