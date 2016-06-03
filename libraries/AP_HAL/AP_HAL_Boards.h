ad293528afe00295357aa5101f5c46db58f0021d
LukeMike <luca.micheletti.laser@gmail.com>
2016-06-03 12:41:42 +0200

    AP_HAL: added some missed definitions

--- libraries/AP_HAL/AP_HAL_Boards.h
+++ libraries/AP_HAL/AP_HAL_Boards.h
@@ -482,6 +482,10 @@
 #define CONFIG_HAL_BOARD_SUBTYPE HAL_BOARD_SUBTYPE_NONE
 #define HAL_STORAGE_SIZE            8192
 #endif
+
+#define LOW     0
+#define HIGH    1
+
 #define HAL_GPIO_A_LED_PIN        25
 #define HAL_GPIO_B_LED_PIN        26
 #define HAL_GPIO_C_LED_PIN        27
